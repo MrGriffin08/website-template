20cec824e334b027d081df30836fb7956c8512ad
MrGriffin08 <americogarza2026@u.northwestern.edu>
2024-10-15 00:24:41 -0500

    initial

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -6,11 +6,11 @@ const AboutMe = () => {
     <div className="flex flex-col items-center py-10 min-h-screen">
       <h1 className="text-6xl font-medium mb-5">About Me</h1>
       {/* image */}
-
+      <img src={stockPhoto} alt="photo" className="w-96 h-56 my-10"/>
       {/*"about me" section */}
-      <div className="bg-blue-200 min-w-56 max-w-96 font-bold flex justify-center px-5"></div>
+      <div className="bg-blue-200 min-w-56 max-w-96 font-bold flex justify-center px-5">TITLE</div>
       {/* bio */}
-      <p className="w-96 px-5"></p>
+      <p className="w-96 px-5">a bunch of words about me</p>
       {/* hobbies and interests */}
       {/* To make a list:
       <ul>
@@ -24,6 +24,12 @@ const AboutMe = () => {
           <li>second</li>
         </ul>
       </div> */}
+      <div>
+        <ul>
+          <li>First thing</li>
+          <li>second thing</li>
+        </ul>
+      </div>
     </div>
   );
 };
